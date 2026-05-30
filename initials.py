2d9bf2ce8d1d985221b9af428b4d903128bf9d5f
Anakwa Ethel Sakyibea <2425401589@live.gctu.edu.gh>
2026-05-30 21:25:26 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Ethel Sakyibea Anakwa
+# STUDENT ID: 2425401589
+# Email: 2425401589@live.gctu.edu.gh
+# Grader: Ethel
+#
+# On my honor, Ethel Sakyibea Anakwa, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+
+    print()
+    print("...ESA")
+    print()
+
+    print("...EEEEEEEEEE....SSSSSSSSSS....AAAAAAAAAA")
+    print("...EEEEEEEEEE....SSSSSSSSSS....AAAAAAAAAA")
+    print("...EEE...........SSS...........AAA....AAA")
+    print("...EEE...........SSS...........AAA....AAA")
+    print("...EEEEEE.........SSSSSS.......AAAAAAAAAA")
+    print("...EEEEEE..........SSSSSS......AAAAAAAAAA")
+    print("...EEE................SSS......AAA....AAA")
+    print("...EEE................SSS......AAA....AAA")
+    print("...EEEEEEEEEE....SSSSSSSSSS....AAA....AAA")
+    print("...EEEEEEEEEE....SSSSSSSSSS....AAA....AAA")
+
+    print()
 
 
 main()
